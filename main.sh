a33d5562f166a50281a1c5e4b10136804566bd1c
xf-zhao <xfz.zhao@gmail.com>
2024-04-03 05:25:54 +0200

    update

--- main.sh
+++ main.sh
@@ -1,5 +1,6 @@
-for i in $(seq 1 3);
+for i in $(seq 1 10);
 do
-    python3 learn.py seed=0 model=gpt-3.5-turbo-0125 &>/dev/null &
-    sleep 5
+    # python3 learn.py seed=0 model=gpt-3.5-turbo-0125 &>/dev/null &
+    python3 learn.py seed=0 model=gpt-3.5-turbo-0125
+    sleep 10
 done
