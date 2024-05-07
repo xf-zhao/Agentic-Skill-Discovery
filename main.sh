4f3e0c0d307554f4ac6ced87948c64e905f01b89
xf-zhao <xfz.zhao@gmail.com>
2024-05-07 14:25:25 +0200

    seqential

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
-for i in $(seq 1 2);
+for i in $(seq 1 5);
 do
-    python3 learn.py task=null precedents=null seed=0 &>/dev/null &
-    # python3 learn.py seed=0 model=gpt-3.5-turbo-0125
+    # python3 learn.py task=null precedents=null seed=0 &>/dev/null &
+    python3 learn.py task=null precedents=null seed=0
     sleep 10
 done
